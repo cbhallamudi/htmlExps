276a120d1cf2c9bb35a01defa58c4580fe03e26c
cbhallamudi <36947619+cbhallamudi@users.noreply.github.com>
2021-09-02 20:16:25 +0530

    encryption_experiment

--- js/password_enc_dec.js
+++ js/password_enc_dec.js
@@ -3,11 +3,28 @@ document.getElementById("encrypt").addEventListener("click",function(){
 
 	let normalPwd = document.getElementById("pwd").value;
 	var pwdArray = [];
+	var randRev, finalRev = 0;
 	let randNum = Math.floor(Math.random() * (999-100) + 100);
 
-	let encyNum = function(){
-		
-	}
+	console.log(randNum);
+
+	var encyNum = function(){
+		while (randNum > 0){
+			randRev = randNum%10;
+			if(randRev == 0){
+				finalRev = finalRev * 10 + randRev.toString();
+			}else{
+				finalRev = finalRev * 10 + randRev;
+			}
+			console.log("RandRev = "+randRev+", finalRev = "+finalRev);
+			
+			randNum = parseInt(randNum/10);
+			// console.log(randRev+"_"+randNum+"_"+finalRev);
+		}
+		return finalRev;
+		// return "Hello";
+	}();
+
 
 	for(var a = 0; a < normalPwd.length; a++){
 		pwdArray.push(normalPwd[a]);
@@ -15,7 +32,7 @@ document.getElementById("encrypt").addEventListener("click",function(){
 
 	var pwdString = pwdArray.join("_");
 
-	console.log(randNum);
+	console.log(encyNum);
 	console.log(pwdString);
 
 
